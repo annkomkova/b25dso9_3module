e86ab804a52d6af6e7e2a45f6e8096165c8d5b62
Anna Komkova <ann.komkova98@gmail.com>
2026-01-20 14:16:13 +0300

    основы JS

---
+++ theory.js
@@ -0,0 +1,32 @@
+// Основы JS #3.1
+
+// console.log('привет, как дела?')
+
+// let userName = 'Иван',
+//   userAge = 32,
+//   userWork = 'бариста',
+//   apple = '🍎'
+
+// console.log(userName, userAge, userWork, apple)
+
+// let number = '10n'
+// console.log(number / 2)
+
+// const int = 1234567890123456789012345678901234567890n
+
+// let userName = 'petya',
+//   stroke = `привет, ${userName}, как день?`
+
+// console.log(stroke)
+
+// let boolean = 10 < 3
+// console.log(boolean)
+
+// let text = null
+// console.log(text)
+
+// alert('привет!')
+// prompt('как ощущения от изучения кода?', 'ну норм')
+
+// let result = prompt('как ощущения от изучения кода?', 'ну норм')
+// alert(`здорово, что тебе ${result}`)
