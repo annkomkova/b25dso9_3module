adb03a6c1eab0d53f4d31560632ab1dd1edf4579
Anna Komkova <ann.komkova98@gmail.com>
2026-02-03 18:30:55 +0300

    интерактив в js и css

---
+++ main/javascripts/scripts.js
@@ -0,0 +1,64 @@
+moveProgressBar()
+// hideModalWindow()
+drawSVG()
+
+function drawSVG() {
+  let star = document.querySelector('.svgLine')
+  let length = star.getTotalLength()
+
+  star.style.strokeDasharray = length
+  star.style.strokeDashoffset = length
+
+  window.addEventListener('scroll', drawing)
+
+  function drawing() {
+    let scrollPercent =
+      (document.body.scrollTop + document.documentElement.scrollTop) /
+      (document.documentElement.scrollHeight -
+        document.documentElement.clientHeight)
+
+    let draw = length * scrollPercent
+
+    star.style.strokeDashoffset = length - draw
+  }
+}
+
+function moveProgressBar() {
+  let button = document.querySelector('.progressButton')
+  let i = 0
+
+  button.addEventListener('click', () => {
+    if (i == 0) {
+      i = 1
+
+      let elem = document.querySelector('.progress')
+      let number = document.querySelector('.progressNumber')
+      let width = 1
+
+      function frame() {
+        if (width < 100) {
+          width++
+          elem.style.width = width + '%'
+          elem.style.opacity = 1
+          number.innerText = width + '%'
+        } else {
+          i = 0
+        }
+      }
+
+      setInterval(frame, 50)
+    }
+  })
+}
+
+function hideModalWindow() {
+  let section = document.querySelector('#modalWindowSection')
+
+  setTimeout(() => {
+    section.style.display = 'flex'
+  }, 3000)
+
+  section.addEventListener('click', () => {
+    section.style.display = 'none'
+  })
+}
